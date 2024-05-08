0325abc75d7325ffbb157c35ced01bf790d592af
MarcosVinicio <marcosalmeida94334@gmail.com>
2024-05-08 10:15:30 -0300

    Editar veiculo

--- DesenvolvimentoWeb/Controllers/VeiculosController.cs
+++ DesenvolvimentoWeb/Controllers/VeiculosController.cs
@@ -41,6 +41,52 @@ namespace DesenvolvimentoWeb.Controllers {
             return View(veiculo);
         }
 
+
+
+        // editar dados
+        // GET
+        public async Task<IActionResult> Edit(int? id) {
+                
+            if(id == null) {
+                return  NotFound();
+            }
+
+            var dados = await _context.Veiculos.FindAsync(id);
+
+
+            if(dados == null) {
+                return NotFound();
+            }
+
+            return View(dados);
+
+        }
+
+
+
+        // editar dados
+        // POST
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Veiculo veiculo) {
+                
+            if(id != veiculo.Id) {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid) {
+                _context.Veiculos.Update(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+
+
+
+
     }
 
 }
