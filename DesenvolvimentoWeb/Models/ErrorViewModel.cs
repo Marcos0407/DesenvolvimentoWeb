7efaf7dad4edf8988103cb01964d7637929de3bb
MarcosVinicio <marcosalmeida94334@gmail.com>
2024-05-07 23:31:40 -0300

    Adicionar arquivos de projeto.

---
+++ DesenvolvimentoWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace DesenvolvimentoWeb.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
