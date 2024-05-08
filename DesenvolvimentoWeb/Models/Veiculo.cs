45d0ad28997568326e02a22716509b6a490260a9
MarcosVinicio <marcosalmeida94334@gmail.com>
2024-05-08 09:09:08 -0300

    Página Veiculo criado com sucesso!

--- DesenvolvimentoWeb/Models/Veiculo.cs
+++ DesenvolvimentoWeb/Models/Veiculo.cs
@@ -7,19 +7,26 @@ namespace DesenvolvimentoWeb.Models {
 
     public class Veiculo {
 
+
         [Key]
         public int Id { get; set; }
+
 
         [Required(ErrorMessage = "Obrigatório informar o Nome!")]
         public string Nome { get; set; }
 
+
         [Required(ErrorMessage = "Obrigatório informar a Placa!")]
         public string Placa { get; set; }
 
+
         [Required(ErrorMessage = "Obrigatório informar o Ano de Fabricação!")]
+        [Display(Name = "Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
 
+
         [Required(ErrorMessage = "Obrigatório informar o Ano do Modelo!")]
+        [Display(Name = "Ano do Modelo")]
         public  int AnoModelo { get; set; }
 
 
